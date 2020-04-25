070f1d1161d1199d8f411b0bc04f7600b355b17b
udhay018777 <63841073+udhay018777@users.noreply.github.com>
2020-04-25 16:33:33 +0530

    Update deploy.py

--- userbot/plugins/deploy.py
+++ userbot/plugins/deploy.py
@@ -35,7 +35,7 @@ async def _(event):
 
     animation_chars = [
         
-            "**Heroku Connecting To Latest Github Build (indianbhaiya/IndianBot)**",
+            "**Heroku Connecting To Latest Github Build (udhay018777/reaperuserbot)**",
             "**Build started by user** **{DEFAULTUSER}**",
             "**Deploy** `535a74f0` **by user** **{DEFAULTUSER}**",
             "**Restarting Heroku Server...**",
